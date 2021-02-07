3f55db5613b5d8a3830cc2f024e6e7335b463657
Lina Ma <lina.ma0616@gmail.com>
2021-02-07 15:01:35 -0700

    charts and data

--- js/app.js
+++ js/app.js
@@ -1,25 +1,54 @@
-// grab references to the input element and the output div
-// @TODO: YOUR CODE HERE
 
-// Function to reverse a string
-function reverseString(str) {
-  return str.split("").reverse().join("");
+// create the function to get the necessary data
+function getInfo(name) {
+
+  // read the json file to get data
+  d3.json("continent-110m.json").then((data)=> {
+      // get the metadata info for the demographic panel
+      var properties = data.properties;
+
+      console.log(properties)
+
+      // filter meta data info by id
+      var result = properties.filter(property => property.name.toString() === name)[0];
+
+      // select demographic panel to put data
+      var CountryInfo = d3.select("#continent-110m-properties");
+      
+      // empty the demographic info panel each time before getting new id info
+      CountryInfo.html("");
+
+      // grab the necessary demographic data data for the id and append the info to the panel
+      Object.entries(result).forEach((key) => {   
+          CountryInfo.append("h5").text(key[0].toLowerCase() + ": " + key[1] + "\n");    
+      });
+  });
 }
 
-// Function to handle input change
-function handleChange(event) {
-  // grab the value of the input field
-  // @TODO: YOUR CODE HERE
-
-  // clear the existing output
-  // @TODO: YOUR CODE HERE
-
-  // reverse the input string
-  // @TODO: YOUR CODE HERE
-
-  // Set the output text to the reversed input string
-  // @TODO: YOUR CODE HERE
+// create the function for the change event
+function optionChanged(name) {
+  getPlot(name);
+  getInfo(name);
 }
 
-// Attach an event to detect changes to the input field.
-// @TODO: YOUR CODE HERE
+// create the function for the initial data rendering
+function init() {
+  // select dropdown menu 
+  var dropdown = d3.select("#Dataset");
+
+  // read the data 
+  d3.json("continent-110m.json").then((data)=> {
+      console.log(data)
+
+      // get the id data to the dropdwown menu
+      data.names.forEach(function(name) {
+          dropdown.append("option").text(name).property("value");
+      });
+
+      // call the functions to display the data and the plots to the page
+      getPlot(data.names[0]);
+      getInfo(data.names[0]);
+  });
+}
+
+init();
